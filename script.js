5ab6942fceb0692c41ea4f198acc02ea7619dfc2
Adriana <drib.yamashita@gmail.com>
2022-11-04 00:22:07 -0300

    traço na task pela checkbox

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let array = [
     {id:create_UUID(), task:'Limpar a Casa'},
     {id:create_UUID(), task:'Fazer Compras do Super'}
 ]
+
 // cria um UUID
 function create_UUID() {
     let dt = new Date().getTime();
@@ -17,7 +18,6 @@ function create_UUID() {
     });
     return uuid;
 }
-
 
 // variáveis do HTML para trabalhar com DOM
 let btnAddTask = document.querySelector('#btnAddTask');
@@ -36,12 +36,31 @@ function AddWindow() {
     windowEditBack.classList.toggle('abrir'); // para deixar o fundo preto opaco
 }
 
+// 
+function check(idTask){
+    const cb = document.querySelector('#check' + idTask);
+    const task = document.getElementById('span' + idTask);
+    
+    if(cb.checked) {
+        task.style.textDecoration = 'line-through';
+    } 
+    else if(!cb.check) {
+        task.style.textDecoration = 'none';
+    }
+    
+}
+
 // adiciona a tarefa quando clicar no Salvar
 btnAddTask.addEventListener('click', () => {
-    const taskValue = inputNewTask.value; //varivel para guardar o valor que está no meu input
-    if(taskValue) {array.push({id:create_UUID(), task:taskValue})} // if para não adicionar task vazia
-    else { alert("Precisa digitar um nome pra tarefa") 
-    return}
+    const taskValue = inputNewTask.value; // varivel para guardar o valor que está no meu input
+
+    if(taskValue) { // if para não adicionar task vazia
+        array.push( {id:create_UUID(), task:taskValue} )
+    }
+    else {
+        alert("Precisa digitar um nome para tarefa") 
+        return
+    }
 
     inputNewTask.value = "" // para quando clicar no botão adicionar, o campo input fica vazio
 
@@ -49,36 +68,33 @@ btnAddTask.addEventListener('click', () => {
     AddWindow(); // para voltar a tela inicial e sair do modal 
 })
 
-
 // Tafera para quando o botão EDITAR é acionado
 function edit(idTask){
+    document.getElementById("inputTaskNameEdit").dataset.idTask = idTask; // Pegamos o id da tafera para editar
+    let li = document.getElementById('' + idTask + ''); // Pegamos o li da tafera pelo Id
 
-    document.getElementById("inputTaskNameEdit").dataset.idTask = idTask; //Pegamos o id da tafera para editar
-    let li = document.getElementById(''+idTask+''); //Pegamos o li da tafera pelo Id
-
-    if (li){
+    if(li) {
         // idTarefaEdicao.innerHTML = '#' +idTask;  // Ativar caso queira saber qual o ID da tarefa no popup editar
-         inputTaskNameEdit.value = li.innerText; // coloca o nome da tarefa atuar no input de editar
+        inputTaskNameEdit.value = li.innerText; // coloca o nome da tarefa atuar no input de editar
         shiftWindow();
     }
-    
 }
 
 // Tarefa para atualizar o valor editado quando clicar em Atualizar
 function update() {
     let updte = document.getElementById("inputTaskNameEdit").value; // pega o valor digitado no input editar
-    idTask = document.getElementById("inputTaskNameEdit").dataset.idTask; //Pegamos o id da tafera para editar
+    idTask = document.getElementById("inputTaskNameEdit").dataset.idTask; // Pegamos o id da tafera para editar
    
     // Descobir qual é o índice da tarefa a ser editada
     const iTODO = array.findIndex(({id}) => id == idTask)
 
     array[iTODO].task = updte; // pegamos o array no indice a ser editado, e passamos o novo valor capturado no update
     shiftWindow() // sai do modal Editar
-    showResult() //mostra a lista de tarefas, já com o array editado na tela 
-  }
+    showResult() // mostra a lista de tarefas, já com o array editado na tela 
+}
 
-    // Ao clicar no botão fechar do modal Editar ele chama a funcao alternar janela para voltar a tela inicial
-    btnEditWindowClose.addEventListener('click', (e) => {
+// Ao clicar no botão fechar do modal Editar ele chama a funcao alternar janela para voltar a tela inicial
+btnEditWindowClose.addEventListener('click', (e) => {
     shiftWindow();
 });
 
@@ -89,15 +105,12 @@ function shiftWindow() {
     windowEditBack.classList.toggle('abrir');
 }
 
-
 // deleta a tarefa
 function delet(idTask) {
     let confirmation = window.confirm('Tem certeza que deseja excluir essa tarefa?')
 
-
     if (confirmation) {
-        
-        //indentificamos qual o índice 
+        // indentificamos qual o índice 
         const iTODO = array.findIndex(({id}) => id == idTask)
 
         // Remove o Li do html
@@ -105,10 +118,8 @@ function delet(idTask) {
         taskRemove.parentNode.removeChild(taskRemove);
 
         return array.splice(iTODO, 1); // retorna a array sem o elemento excluido
-        
     }     
 }
-
 
 // Atualizar interface/tela com os objetos alterados do aray 
 function showResult() {
@@ -117,32 +128,31 @@ function showResult() {
     ul.innerHTML = null // impede de repetir todas as tarefas quando entra uma nova
 
     array.forEach(function (taskIn) {
-        // cra o li de cada TODO
+        // cria o li de cada TODO
         let li = document.createElement("li");
         li.id = taskIn.id;
 
-
         // cria o ícone do check
         let decoration = document.createElement('input')
-        decoration.setAttribute("type", "checkbox")
+        decoration.setAttribute("type", "checkbox");
+        decoration.id = "check" + taskIn.id;
+        decoration.setAttribute("onclick", `check('${taskIn.id}')`);
 
         // cria o span de cada TODO
         let span = document.createElement('span');
         span.classList.add('taskList');
-        span.id = taskIn.id;
+        span.id = "span" + taskIn.id;
         span.innerHTML = taskIn.task;
 
-        // cra o div de cada TODO
+        // cria o div de cada TODO
         let div = document.createElement('div');
 
-        
         // cria o button (edit) de cada TODO
         let btnEdit = document.createElement('button');
         btnEdit.classList.add('btnAction'); 
         btnEdit.innerHTML = '<img src="Assets/pencil.svg">';
         btnEdit.setAttribute("onclick", `edit('${taskIn.id}')`);
         btnEdit.setAttribute("id", taskIn.id);
-
 
         // cria o button (delete) de cada TODO
         let btnTrash = document.createElement('button');
@@ -158,13 +168,8 @@ function showResult() {
         li.appendChild(div); // insere o div dentro do ul de cima
         div.appendChild(btnTrash);// insere o btnTrash dentro do div de cima
         div.appendChild(btnEdit);// insere o btnEdit dentro do div de cima
-
     })
 }
 
-
 // Executa Função para Mostrar o TODO
 showResult();
-
-
-
